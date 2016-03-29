4c1f0f895975e52a8eca6c348c0e708c6c2fca4a
svetkapanenko <panenko200886@gmail.com>
2016-03-29 17:11:44 +0300

    assembled layout with a folder ./dist, draft

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,97 @@
-var gulp = require('gulp'),
- sass = require('gulp-sass'),
- watch = require('gulp-watch'),
- concat = require('gulp-concat'),
- autoprefixer = require('gulp-autoprefixer'),
- browserSync = require('browser-sync'),
- uglify = require('gulp-uglify'),
- imagemin = require('gulp-imagemin'),
- minifycss = require('gulp-minify-css');
- 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
+var reload = browserSync.reload;
 
 var markup = './markup';
-var dist = './dist';
+var dist = './dist'
 
 var markupPaths = {
-    html: [markup + '/**/*.html'],
-    js: [markup + '/assets/js/**/*.js'],
-    scss: [markup + '/assets/scss/**/*.scss']
+	html: [ markup + '/*.html'],
+	scss: [ markup + '/assets/scss/**/*.scss'],
+	js: [markup + '/assets/js/**/*.js'],
+  fonts: [markup + '/assets/fonts/**/*.*'],
+  image:[markup + '/assets/images/**/*.+(png|jpg|svg)']
 }
 
-gulp.task('html', function() {
-    return gulp.src(markupPaths.html)
-        .pipe(gulp.dest('./dist'))
-         .pipe(browserSync.reload({
-            stream: true
-        }));
+var distPaths = {
+	css:[ dist + '/assets/css']
+}
+
+gulp.task('html', function () {
+  return gulp.src(markupPaths.html)
+    .pipe(gulp.dest('./dist'))
+    .pipe(reload({
+    	stream: true
+    }));
 });
 
-gulp.task('js', function() {
-    return gulp.src(markupPaths.js)
-        .pipe(uglify())
-        .pipe(concat('assets.js'))
-        .pipe(gulp.dest('./dist/assets/js'))
-         .pipe(browserSync.reload({
-            stream: true
-        }));
+gulp.task('js', function () {
+  return gulp.src(markupPaths.js)
+
+    .pipe(gulp.dest('./dist/assets/js'))
+    .pipe(reload({
+    	stream: true
+    }));
 });
 
-gulp.task('sass', function() {
-    return gulp.src(markupPaths.scss)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(minifycss())
-        .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-        }))
-        .pipe(gulp.dest('./dist/assets/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+gulp.task('sass', function () {
+  return gulp.src(markupPaths.scss)
+  	.pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+  			browsers: ['last 2 versions'],
+			cascade: false
+  	}))
+  	.pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/assets/css'))
+    .pipe(reload({
+    	stream: true
+    }));
 });
-
-gulp.task('images',  function () {
-    return gulp.src('.markup/assets/images/**/*.+(png|jpg|svg)')
-        .pipe(imagemin({
-            multipass: true,
-            interlaced: true,
-            svgoPlugins: [{removeViewBox: false}],
-            optimizationLevel: 3
-        }))
-        .pipe(gulp.dest('./dist/assets/images'))
-});
-
-gulp.task('fonts', function() {
-    return gulp.src('.markup/assets/fonts/**/*.*')
+ 
+ gulp.task('fonts', function() {
+    return gulp.src(markupPaths.fonts)
     .pipe(gulp.dest('./dist/assets/fonts'))
 })
+  gulp.task('images', function() {
+    return gulp.src(markupPaths.image)
+    .pipe(gulp.dest('./dist/assets/images'))
+})
 
-
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
+gulp.task('browserSync', function() {
+  browserSync({
+   server: {
             baseDir: './dist'
-      },
-
-    });
-});
-gulp.task('watch', function() {
-    watch(markupPaths.html, function() {
-        gulp.start('html');
-    });
-    watch(markupPaths.scss, function() {
-        gulp.start('sass');
-    });
-    watch(markupPaths.js, function() {
-        gulp.start('js');
-    });
+        },
+        // logConnections: true,
+        // debugInfo: true,
+        // injectChanges: false,
+        port: 3004,
+        open: true,
+        browser: 'default',
+        startPath: '/index.html'
+        // notify: true,
+        // reloadOnRestart: true
+  });
 });
 
+gulp.task('watch', function () {
+  watch(markupPaths.html, function(){
+  	gulp.start('html');
+  });
+  watch(markupPaths.scss, function(){
+  	gulp.start('sass');
+  });
+  watch(markupPaths.js, function(){
+  	gulp.start('js');
+  });
+});
 
-gulp.task('default', ['watch', 'browser-sync','html','sass','js']);
-
-gulp.task('go',['fonts','images']);
-
-
+gulp.task('dev', ['watch', 'browserSync', 'html','sass','js', 'fonts','images']);
 
